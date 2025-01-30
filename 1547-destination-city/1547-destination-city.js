764e067e90683c8cc5f7d0590d83b2ff46036d7e
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-01-30 09:42:14 +0530

    Time: 2 ms (56.91%), Space: 49.4 MB (99.2%) - LeetHub

---
+++ 1547-destination-city/1547-destination-city.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+var destCity = function(paths) {
+    let t=paths[0][1]
+    for(let i=1;i<paths.length;i++){
+    for(let j=1;j<paths.length;j++){
+        if(t==paths[j][0]){
+        t=paths[j][1]
+        }
+    }
+    }
+    return t
+};
