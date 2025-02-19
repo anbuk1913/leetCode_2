8e13ecf6afc67618fe0948bb829d015e4161b361
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-02-19 10:50:03 +0530

    Time: 1762 ms (10.1%), Space: 63.5 MB (5.05%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.js
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.js
@@ -8,18 +8,44 @@ var findEvenNumbers = function(digits) {
     for(let i=0;i<digits.length;i++){
         for(let j=i+1;j<digits.length;j++){
             for(let k=j+1;k<digits.length;k++){
-                let a=digits[i]*100+digits[j]*10+digits[k]
-                let b=digits[i]*100+digits[k]*10+digits[j]
-                let c=digits[j]*100+digits[i]*10+digits[k]
-                let d=digits[j]*100+digits[k]*10+digits[i]
-                let e=digits[k]*100+digits[i]*10+digits[j]
-                let f=digits[k]*100+digits[j]*10+digits[i]
-                if(a%2==0 && a>99 && !arr.includes(a))arr.push(a)
-                if(b%2==0 && b>99 && !arr.includes(b))arr.push(b)
-                if(c%2==0 && c>99 && !arr.includes(c))arr.push(c)
-                if(d%2==0 && d>99 && !arr.includes(d))arr.push(d)
-                if(e%2==0 && e>99 && !arr.includes(e))arr.push(e)
-                if(f%2==0 && f>99 && !arr.includes(f))arr.push(f)
+                if(digits[i]==digits[j]){
+                    let a=digits[i]*100+digits[j]*10+digits[k]
+                    let b=digits[i]*100+digits[k]*10+digits[j]
+                    let e=digits[k]*100+digits[i]*10+digits[j]
+                    if(a%2==0 && a>99 && !arr.includes(a))arr.push(a)
+                    if(b%2==0 && b>99 && !arr.includes(b))arr.push(b)
+                    if(e%2==0 && e>99 && !arr.includes(e))arr.push(e)
+                } else if(digits[j]==digits[k]){
+                    let a=digits[i]*100+digits[j]*10+digits[k]
+                    let c=digits[j]*100+digits[i]*10+digits[k]
+                    let d=digits[j]*100+digits[k]*10+digits[i]
+                    if(a%2==0 && a>99 && !arr.includes(a))arr.push(a)
+                    if(c%2==0 && c>99 && !arr.includes(c))arr.push(c)
+                    if(d%2==0 && d>99 && !arr.includes(d))arr.push(d)
+                } else if(digits[i]==digits[k]){
+                    let a=digits[i]*100+digits[j]*10+digits[k]
+                    let b=digits[i]*100+digits[k]*10+digits[j]
+                    let c=digits[j]*100+digits[i]*10+digits[k]
+                    if(a%2==0 && a>99 && !arr.includes(a))arr.push(a)
+                    if(b%2==0 && b>99 && !arr.includes(b))arr.push(b)
+                    if(c%2==0 && c>99 && !arr.includes(c))arr.push(c)
+                } else if(digits[i] == digits [j] && digits[j] == digits[k]){
+                    let a=digits[i]*100+digits[j]*10+digits[k]
+                    if(a%2==0 && a>99 && !arr.includes(a))arr.push(a)
+                } else {
+                    let a=digits[i]*100+digits[j]*10+digits[k]
+                    let b=digits[i]*100+digits[k]*10+digits[j]
+                    let c=digits[j]*100+digits[i]*10+digits[k]
+                    let d=digits[j]*100+digits[k]*10+digits[i]
+                    let e=digits[k]*100+digits[i]*10+digits[j]
+                    let f=digits[k]*100+digits[j]*10+digits[i]
+                    if(a%2==0 && a>99 && !arr.includes(a))arr.push(a)
+                    if(b%2==0 && b>99 && !arr.includes(b))arr.push(b)
+                    if(c%2==0 && c>99 && !arr.includes(c))arr.push(c)
+                    if(d%2==0 && d>99 && !arr.includes(d))arr.push(d)
+                    if(e%2==0 && e>99 && !arr.includes(e))arr.push(e)
+                    if(f%2==0 && f>99 && !arr.includes(f))arr.push(f)
+                }
             }
         }
     }
