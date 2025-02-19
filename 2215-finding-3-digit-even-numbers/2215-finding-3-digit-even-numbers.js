43cc54fec2e7c109d29668d66cf9a34cf8877751
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-02-19 11:28:47 +0530

    Time: 324 ms (18.18%), Space: 89.9 MB (5.05%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.js
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.js
@@ -4,51 +4,16 @@
  */
 var findEvenNumbers = function(digits) {
     let arr=[]
-    if(digits.length<3)return []
     for(let i=0;i<digits.length;i++){
-        for(let j=i+1;j<digits.length;j++){
-            for(let k=j+1;k<digits.length;k++){
-                if(digits[i]==digits[j]){
-                    let a=digits[i]*100+digits[j]*10+digits[k]
-                    let b=digits[i]*100+digits[k]*10+digits[j]
-                    let e=digits[k]*100+digits[i]*10+digits[j]
-                    if(a%2==0 && a>99 && !arr.includes(a))arr.push(a)
-                    if(b%2==0 && b>99 && !arr.includes(b))arr.push(b)
-                    if(e%2==0 && e>99 && !arr.includes(e))arr.push(e)
-                } else if(digits[j]==digits[k]){
-                    let a=digits[i]*100+digits[j]*10+digits[k]
-                    let c=digits[j]*100+digits[i]*10+digits[k]
-                    let d=digits[j]*100+digits[k]*10+digits[i]
-                    if(a%2==0 && a>99 && !arr.includes(a))arr.push(a)
-                    if(c%2==0 && c>99 && !arr.includes(c))arr.push(c)
-                    if(d%2==0 && d>99 && !arr.includes(d))arr.push(d)
-                } else if(digits[i]==digits[k]){
-                    let a=digits[i]*100+digits[j]*10+digits[k]
-                    let b=digits[i]*100+digits[k]*10+digits[j]
-                    let c=digits[j]*100+digits[i]*10+digits[k]
-                    if(a%2==0 && a>99 && !arr.includes(a))arr.push(a)
-                    if(b%2==0 && b>99 && !arr.includes(b))arr.push(b)
-                    if(c%2==0 && c>99 && !arr.includes(c))arr.push(c)
-                } else if(digits[i] == digits [j] && digits[j] == digits[k]){
-                    let a=digits[i]*100+digits[j]*10+digits[k]
-                    if(a%2==0 && a>99 && !arr.includes(a))arr.push(a)
-                } else {
-                    let a=digits[i]*100+digits[j]*10+digits[k]
-                    let b=digits[i]*100+digits[k]*10+digits[j]
-                    let c=digits[j]*100+digits[i]*10+digits[k]
-                    let d=digits[j]*100+digits[k]*10+digits[i]
-                    let e=digits[k]*100+digits[i]*10+digits[j]
-                    let f=digits[k]*100+digits[j]*10+digits[i]
-                    if(a%2==0 && a>99 && !arr.includes(a))arr.push(a)
-                    if(b%2==0 && b>99 && !arr.includes(b))arr.push(b)
-                    if(c%2==0 && c>99 && !arr.includes(c))arr.push(c)
-                    if(d%2==0 && d>99 && !arr.includes(d))arr.push(d)
-                    if(e%2==0 && e>99 && !arr.includes(e))arr.push(e)
-                    if(f%2==0 && f>99 && !arr.includes(f))arr.push(f)
-                }
+        for(let j=0;j<digits.length;j++){
+            if(i==j)continue
+            for(let k=0;k<digits.length;k++){
+                if(i==k || j==k )continue
+                let a=digits[i]*100+digits[j]*10+digits[k]
+                if(digits[i] !=0 && a%2==0)arr.push(a)
             }
         }
     }
-    arr.sort((a,b)=>a-b)
-    return arr
+    arr =[...new Set(arr)]
+    return arr.sort((a,b)=>a-b)
 };
