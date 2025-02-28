30f930ee8612fdd7d221498d7b34b8bd09115546
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-02-28 11:54:05 +0530

    Time: 0 ms (100%), Space: 53.6 MB (23.08%) - LeetHub

---
+++ 0770-couples-holding-hands/0770-couples-holding-hands.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} row
+ * @return {number}
+ */
+var minSwapsCouples = function(row) {
+    let count=0
+    for(let i=1;i<row.length;i++){
+        if(row[i-1]%2==0){
+            let num = row[i-1]
+            if(row[i] == num+1){
+                i++
+                continue
+            } else {
+                count++
+                let tem = i
+                while(row[tem]!=num+1){
+                tem++
+                }
+                let t = row[i]
+                row[i] = row[tem]
+                row[tem] = t
+                i++
+            }
+        } else {
+        let num = row[i-1]
+        if(row[i] == num-1){
+            i++
+            continue
+        } else {
+            count++
+            let tem = i
+            while(row[tem]!=num-1){
+                tem++
+            }
+            let t = row[i]
+            row[i] = row[tem]
+            row[tem] = t
+            i++
+        }
+        }
+    }
+    return count
+};
