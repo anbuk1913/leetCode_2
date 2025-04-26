0392022538dcff61d5492295d85557424136e576
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-04-26 17:26:08 +0530

    Time: 0 ms (100%), Space: 54.5 MB (83.46%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.js
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.js
@@ -3,12 +3,73 @@
  * @return {number}
  */
 var maximumCount = function(nums) {
-    let posCount = 0
-    let negCount = 0
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]<0)negCount++
-        else if(nums[i]>0)posCount++
+    let left = 0
+    let right = nums.length-1
+
+    let mid
+    
+    if((nums[left]<0 && nums[right]<0) || (nums[left]>0 && nums[right]>0)){
+        return nums.length
     }
-    if(posCount>negCount)return posCount
-    else return negCount
+
+    while(left<=right){
+        mid = Math.floor((left+right)/2)
+        if(nums[mid]==0){
+            break
+        }
+        else if(nums[mid]<0){
+            left = mid+1
+        }
+        else if(nums[mid]>0){
+            right = mid-1
+        }
+    }
+
+    if(nums[mid]==0){
+        left = right = mid
+        while(nums[left]==0 && left!=0){
+            left--
+        }
+        while(nums[right]==0 && right!=nums.length-1){
+            right++
+        }
+        let max = 0
+        if(left+1>nums.length-right){
+            max = left+1
+        } else if(left+1<nums.length-right) {
+            max = nums.length-right
+        }
+        if(left+1==nums.length-right && nums[0]!=0){
+            max = left+1
+        }
+        return max
+    } else {
+        if(nums[mid]<0){
+            let max = 0
+            left = mid
+            right = mid+1
+            if(left+1>nums.length-right){
+                max = left+1
+            } else if(left+1<nums.length-right) {
+                max = nums.length-right
+            }
+            if(left+1==nums.length-right && nums[0]!=0){
+                max = left+1
+            }
+            return max
+        } else {
+            let max = 0
+            left = mid-1
+            right = mid
+            if(left+1>nums.length-right){
+                max = left+1
+            } else if(left+1<nums.length-right) {
+                max = nums.length-right
+            }
+            if(left+1==nums.length-right && nums[0]!=0){
+                max = left+1
+            }
+            return max
+            }
+    }
 };
