8c332b1814b9258d8c627add4660c223b85a5ba6
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-05-06 13:02:45 +0530

    Time: 4 ms (76.68%), Space: 76.1 MB (73.99%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -10,13 +10,32 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-    let ans = ""
-    let curr = head
-    while(curr){
-        ans+=curr.val
-        curr = curr.next
+    if(!head || !head.next)return true
+    let slow = head
+    let fast = head
+    while(fast.next && fast.next.next){
+        slow = slow.next
+        fast = fast.next.next
     }
-    let tem = ans
-    tem = ans.split("").reverse("").join("")
-    return ans==tem
+    let sec = reverse(slow.next)
+    slow = head
+    while(sec){
+        if(sec.val != slow.val)return false
+        sec = sec.next
+        slow = slow.next
+    }
+    return true
 };
+
+function reverse(head){
+    let curr = head
+    let prev = null
+    let next = null
+    while(curr){
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+    return prev
+}
