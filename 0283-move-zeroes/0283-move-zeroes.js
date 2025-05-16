834a4ec4ece149192bbc5ce92baf846f7abe4c4d
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-05-16 09:18:04 +0530

    Time: 5 ms (30.94%), Space: 60.9 MB (21.58%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,13 +3,10 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let end = 0 ;
-    for(let i = 0 ; i<nums.length ; i++){
+    let tem = 0
+    for(let i=0;i<nums.length;i++){
         if(nums[i]!=0){
-            let temp  = nums[i];
-            nums[i] = nums[end];
-            nums[end] = temp;
-            end++
+            nums[i] = nums[i]+nums[tem]-(nums[tem++]=nums[i])
         }
     }
 };
