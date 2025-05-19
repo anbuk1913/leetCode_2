440d68cd6d880707976f9b922bcac762fb89013f
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-05-19 09:56:43 +0530

    Time: 8 ms (86.91%), Space: 58.6 MB (87.43%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -3,10 +3,36 @@
  * @return {number}
  */
 var maximumProduct = function(nums) {
-    nums.sort((a,b)=>a-b)
-    if(nums[0]*nums[1]*nums[nums.length-1]>nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3]){
-        return nums[0]*nums[1]*nums[nums.length-1]
+    let min1 = Infinity
+    let min2 = Infinity
+    let max1 = -Infinity
+    let max2 = -Infinity
+    let max3 = -Infinity
+    for(let i of nums){
+        if(min1>i){
+            min2 = min1
+            min1 = i
+        }
+        else if(min2>i){
+            min2 = i
+        }
+        if(max1<i){
+            max3 = max2
+            max2 = max1
+            max1 = i
+        }
+        else if(max2<i){
+            max3 = max2
+            max2 = i
+        }
+        else if(max3<i){
+            max3 = i
+        }
+    }
+    console.log(min1,min2,max1,max2,max3)
+    if(min1*min2*max1>max1*max2*max3){
+        return min1*min2*max1
     } else {
-        return nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3]
+        return max1*max2*max3
     }
 };
