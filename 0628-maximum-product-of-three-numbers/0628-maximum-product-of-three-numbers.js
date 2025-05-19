083057f19e04eaec9981b4c09a44b2e5969b239e
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-05-19 09:34:56 +0530

    Time: 43 ms (6.54%), Space: 60.8 MB (12.83%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    nums.sort((a,b)=>a-b)
+    if(nums[0]*nums[1]*nums[nums.length-1]>nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3]){
+        return nums[0]*nums[1]*nums[nums.length-1]
+    } else {
+        return nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3]
+    }
+};
