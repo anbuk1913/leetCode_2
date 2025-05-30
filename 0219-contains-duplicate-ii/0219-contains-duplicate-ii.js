6c82f7dbdf8b08cba5df351ac29facfe769614e7
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-05-30 11:00:46 +0530

    Time: 5 ms (99.76%), Space: 65.4 MB (96.18%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -4,14 +4,16 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
+    let l = 0;
     for(let i=0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
+        for(let j=i+1;j-i<= k && j<nums.length;j++){
             if(nums[i]===nums[j]){
-                if(j-i<=k)return true
-            } else if(j-i>k) {
-                break
+                return true
             }
+            l++;
+            if(l >= 100000) return false;
         }
+
     }
     return false
 };
