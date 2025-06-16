65f94bcf0dcd5e42c28dd123e46761b0bf5c3668
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-06-16 09:41:57 +0530

    Time: 0 ms (100%), Space: 54.8 MB (5.87%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.js
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.js
@@ -12,9 +12,12 @@
 var middleNode = function(head) {
     let slow = head
     let fast = head
-    while(fast && fast.next){
+    while(fast.next && fast.next.next){
         slow = slow.next
         fast = fast.next.next
+    }
+    if(fast.next){
+        slow = slow.next
     }
     return slow
 };
